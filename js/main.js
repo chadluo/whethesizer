d6997d04fad6517ca83147b4d98bdcf728e5b343
chadluo <i@yuki.rocks>
2016-11-23 23:13:48 +1100

    play different sound on different weather

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var synth = new Tone.Synth().toMaster();
+var synth = new Tone.PolySynth(3, Tone.MonoSynth).toMaster();
 
 $('#play').on('click', function () {
     $.getJSON(
         "http://api.openweathermap.org/data/2.5/forecast/daily?q=Sydney&mode=json&units=metric&cnt=1&appid=c989bce9f69b008a0b1e46d4c0acaa80",
         function (result) {
-            $('#board').append(result.list[0].humidity);
-            synth.triggerAttackRelease((result.list[0].weather[0].main === "Rain" ? "C4" : "C3"), "1n");
+            const humidity = result.list[0].humidity;
+            synth.triggerAttackRelease(humidity > 75 ? ["C3", "Eb3", "G3"] : ["C4", "E4", "G4"], "1n");
         });
 });
