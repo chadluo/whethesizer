2612d99fad6cb2d3f38b68fb265058f267cb11a5
Chad'; DROP TABLE REPOSITORIES <chadluo@users.noreply.github.com>
2018-05-09 21:59:35 +1000

    openweathermap -> https

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const c4 = 440;
 const major = [1, Math.pow(2, 1 / 3), Math.pow(2, 7 / 12)];
 const minor = [1, Math.pow(2, 1 / 4), Math.pow(2, 7 / 12)];
 
-const url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Sydney&mode=json&units=metric&cnt=1&appid=c989bce9f69b008a0b1e46d4c0acaa80";
+const url = "https://api.openweathermap.org/data/2.5/forecast/daily?q=Sydney&mode=json&units=metric&cnt=1&appid=c989bce9f69b008a0b1e46d4c0acaa80";
 
 $.getJSON(url, function (result) {
 
